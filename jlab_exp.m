e076f1fe2d97a009b4145b213bb6a2dbb52f99ff
Richard Cui <richard.jie.cui@gmail.com>
2018-05-19 22:05:13 -0700

    add jlab_exp.m

---
+++ jlab_exp.m
@@ -0,0 +1,33 @@
+function add_dir = jlab_exp
+% JLAB_EXP directories of additional experiments for jLab gui
+%
+% Syntax:
+%
+% Input(s):
+%
+% Output(s):
+%
+% Example:
+%
+% Note:
+%   Searchs inside the default folder (jLab/Corrui/Experiments) and
+% additional folders for class names (tag).  The folder name is the tag
+% name.  Put the folders under MatLab pathways.
+%
+% References:
+%
+% See also .
+
+% Copyright 2018 Richard J. Cui. Created: Sat 05/19/2018  9:01:31.718 PM
+% $Revision: 0.1 $  $Date: Sat 05/19/2018  9:01:31.723 PM $
+%
+% 3236 E Chandler Blvd Unit 2036
+% Phoenix, AZ 85048, USA
+%
+% Email: richard.cui@utoronto.ca
+
+add_dir = {'/Users/jiecui/Documents/Jie/Projects/other_projects/gfs_msacc/jlab_exp'};
+
+end % function jlab_exp
+
+% [EOF]
